0b6d35144a3ae5103f15c0826979f45efc032b75
--replace-all <moipav@ya.ru>
2022-07-10 18:26:13 +0800

    add database schema

--- db/QueryBuilder.php
+++ db/QueryBuilder.php
@@ -4,14 +4,15 @@ class QueryBuilder
 {
 //    protected PDO $pdo;
 
-    public function __construct(protected PDO $pdo)
+    public function __construct(
+        protected PDO $pdo
+    )
     {
 
     }
 
     public function getAll(): array
     {
-        debug($this->pdo);
         $stmt = $this->pdo->prepare('SELECT * FROM posts');
         $stmt->execute();
         return $stmt->fetchAll();
---
+++ db/marlin_oop.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3307
+-- Время создания: Июл 10 2022 г., 13:11
+-- Версия сервера: 5.7.33
+-- Версия PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `marlin_oop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `post` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `post`) VALUES
+(1, 'первый пост записанный через phpmyadmin', 'просто пост совершенно ни о чем'),
+(2, 'второй пост записанный через phpmyadmin', 'точно так же просто пост совершенно ни о чем, вместо лорем ипсум');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
